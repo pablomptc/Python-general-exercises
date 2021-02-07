1dba2f2eaa459186b3e5d51d33111a34aa6d136b
Pablo <37776542+pablomptc@users.noreply.github.com>
2021-02-07 18:50:41 +0100

    Update Puntos dentro de un triangulo en un grafico.py

--- Puntos dentro de un triangulo en un grafico.py
+++ Puntos dentro de un triangulo en un grafico.py
@@ -1,91 +1,87 @@
+CALCULATION OF NUMBER OF POINTS WITHIN A TRIANGLE
+
+"""The objective of this exercise is to find the number of points of each type that are inside a triangle.
+Each point is actually a bus stop in a city in Europe. The "X" coordinate is "demand average score" and the 
+"y" coordinate is "offer average score". This two dimensions were calculated previously for each stop from 
+different indicators. Values range from 1 to 5. Each stop can be of 3 types: rural, city and town. And this
+does not depend on the two variables X and Y. And for a question of the project, it was asked to calculate 
+all the points of city, rural and town that had an offer average score less than 3, and that at the same time 
+were in the upper part of the line y = x."""
+
+#Libraries
 import pandas as pd
 import matplotlib as plt
 import numpy as np
-
-a = pd.read_csv("rankings.csv")
-e = pd.read_csv("Scale2.csv")
-k = pd.read_csv("Final.csv")
-
-#Regresion lineal python (FIND M AND  N)
-X = a[["_DEMAND_ AVERAGE SCORE"]].values
-y = a[["_OFFER_ AVERAGE SCORE"]].values
-
-
 from sklearn.linear_model import LinearRegression
-slr = LinearRegression()
-slr.fit(X,y)
-print("Slope: %.3f" % slr.coef_[0])
 
-print("Intercept: %.3f" % slr.intercept_)
 
-#Select the points above the line and below offer
-#Seleccionar los puntos que estén por encima de la linea y debajo de offer= 3
-
-#Slope: 0.507
-#Intercept: 1.487
-
-f = list(zip(b,c,d))
+file = pd.read_csv("Final.csv")
 
 
 #Urban points
-Urban_points = k[k.Scale == "Urban"]
-Urban_points1 = Urban_points[Urban_points["_OFFER_ AVERAGE SCORE"] < 3]
-Ux = Urban_points1["_DEMAND_ AVERAGE SCORE"].tolist()
-Uy = Urban_points1["_OFFER_ AVERAGE SCORE"].tolist()
-Uz = Urban_points1["Scale"].tolist()
-Urban = list(zip(Ux,Uy,Uz))
+urban_points = file[file.Scale == "Urban"]
+urban_points_under_3 = urban_points[urban_points["_OFFER_ AVERAGE SCORE"] < 3]
+urban_D = urban_points_under_3["_DEMAND_ AVERAGE SCORE"].tolist ()
+urban_O = urban_points_under_3["_OFFER_ AVERAGE SCORE"].tolist ()
+urban_S = urban_points_under_3["Scale"].tolist ()
+urban = list (zip (urban_D, urban_O, urban_S))
 
-above_U = []
-under_U = []
+above_u = []
+under_u = []
 
-for i in Urban:
-    if i[1] > (0.507*(i[0])+1.487):
-        above_U.append(i)
+for i in urban:
+    if i[1] > i[0]:
+        above_u.append (i)
     else:
-        under_U.append(i)
-above_U
-
-
-
+        under_u.append (i)
+        
 
 #Town points
-Town_points = k[k.Scale == "Town"]
-Town_points1 = Town_points[Town_points["_OFFER_ AVERAGE SCORE"] < 3]
-Tx = Town_points1["_DEMAND_ AVERAGE SCORE"].tolist()
-Ty = Town_points1["_OFFER_ AVERAGE SCORE"].tolist()
-Tz = Town_points1["Scale"].tolist()
-Town = list(zip(Tx,Ty,Tz))
+town_points = file[file.Scale == "Town"]
+town_points_under_3 = town_points[town_points["_OFFER_ AVERAGE SCORE"] < 3]
+town_D = town_points_under_3["_DEMAND_ AVERAGE SCORE"].tolist ()
+town_O = town_points_under_3["_OFFER_ AVERAGE SCORE"].tolist ()
+town_S = town_points_under_3["Scale"].tolist ()
+town = list(zip(town_D, town_O, town_S))
 
-above_T = []
-under_T = []
+above_t = []
+under_t = []
 
-for i in Town:
-    if i[1] > (0.507*(i[0])+1.487):
-        above_T.append(i)
+for i in town:
+    if i[1] > i[0]:
+        above_t.append(i)
     else:
-        under_T.append(i)
-under_T
+        under_t.append(i)
+under_t
+
 
 #Rural points
-Rural_points = k[k.Scale == "Rural"]
-Rural_points1 = Rural_points[Rural_points["_OFFER_ AVERAGE SCORE"] < 3]
-Rx = Rural_points1["_DEMAND_ AVERAGE SCORE"].tolist()
-Ry = Rural_points1["_OFFER_ AVERAGE SCORE"].tolist()
-Rz = Rural_points1["Scale"].tolist()
-Rural = list(zip(Rx,Ry,Rz))
+rural_points = file[file.Scale == "Rural"]
+rural_points_under_3 = rural_points[rural_points["_OFFER_ AVERAGE SCORE"] < 3]
+rural_D = rural_points_under_3["_DEMAND_ AVERAGE SCORE"].tolist()
+rural_O = rural_points_under_3["_OFFER_ AVERAGE SCORE"].tolist()
+rural_S = rural_points_under_3["Scale"].tolist()
+rural = list(zip(rural_D, rural_O, rural_S))
 
-above_R = []
-under_R = []
+above_r = []
+under_r = []
 
-for i in Rural:
-    if i[1] > (0.507*(i[0])+1.487):
-        above_R.append(i)
+for i in rural:
+    if i[1] > i[0]:
+        above_r.append(i)
     else:
-        under_R.append(i)
+        under_r.append(i)
 
 
 
+# NUMBER OF URBAN POINTS
+u = str(len(above_u))
+print ("The number of rural points is: " + u)
 
- len(above_U)
-len(above_T)
-len(above_R)
+# NUMBER OF TOWN POINTS
+t = str(len(above_t))
+print ("The number of rural points is: " + t)
+
+# NUMBER OF RURAL POINTS
+r = str(len(above_r))
+print ("The number of rural points is: " + r)
